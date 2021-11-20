19e84fc75eb2210c7210e696822d254c5978aaf0
kishoreBandanadam <krrish.kishore02@gmail.com>
2021-11-20 08:30:32 +0530

    good change

--- force-app/main/default/lwc/emailLwc/emailLwc.js
+++ force-app/main/default/lwc/emailLwc/emailLwc.js
@@ -1,3 +1,83 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+import search from "@salesforce/apex/EmailClass.search";
 
-export default class EmailLwc extends LightningElement {}
+export default class EmailLwc extends LightningElement {
+    @track items = [];
+    searchTerm = "";
+    blurTimeout;
+    boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
+    @track selectedValues = [];
+    selectedValuesMap = new Map();
+
+    get hasItems() {
+        return this.items.length;
+    }
+
+    handleInputChange(event) {
+        if (event.target.value.length < 3) {
+            return;
+        }
+
+        this.searchTerm = event.target.value;
+
+        search({ searchString: event.target.value })
+            .then((result) => {
+                console.log("Result", result);
+                this.items = result;
+                if (this.items.length > 0) {
+                    this.boxClass =
+                        "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open";
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+        //this.items = [];
+    }
+
+    handleBlur() {
+        console.log("In onBlur");
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.blurTimeout = setTimeout(() => {
+            this.boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
+            const value = this.searchTerm;
+            if (value !== undefined && value != null && value !== "") {
+                this.selectedValuesMap.set(value, value);
+                this.selectedValues = [...this.selectedValuesMap.keys()];
+            }
+
+            console.log("this.selectedValues***", this.selectedValues);
+            this.searchTerm = "";
+        }, 300);
+    }
+
+    handleRemove(event) {
+        const item = event.target.label;
+        console.log("item", item);
+        this.selectedValuesMap.delete(item);
+        this.selectedValues = [...this.selectedValuesMap.keys()];
+    }
+
+    onSelect(event) {
+        this.searchTerm = "";
+        console.log("In onSelect");
+        let ele = event.currentTarget;
+        let selectedId = ele.dataset.id;
+        console.log("selectedId", selectedId);
+        let selectedValue = this.items.find((record) => record.Id === selectedId);
+        this.selectedValuesMap.set(selectedValue.Email, selectedValue.Name);
+        this.selectedValues = [...this.selectedValuesMap.keys()];
+
+        //As a best practise sending selected value to parent and inreturn parent sends the value to @api valueId
+        let key = this.uniqueKey;
+        const valueSelectedEvent = new CustomEvent("valueselect", {
+            detail: { selectedId, key }
+        });
+        this.dispatchEvent(valueSelectedEvent);
+
+        if (this.blurTimeout) {
+            clearTimeout(this.blurTimeout);
+        }
+        this.boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
+    }
+}
