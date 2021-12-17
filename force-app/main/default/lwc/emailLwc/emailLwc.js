0fc37da0fb00cb51f09c87b73e8248c70d8fbcc4
kishoreBandanadam <krrish.kishore02@gmail.com>
2021-12-17 19:49:19 +0530

    Email Lwc

--- force-app/main/default/lwc/emailLwc/emailLwc.js
+++ force-app/main/default/lwc/emailLwc/emailLwc.js
@@ -1,83 +1,121 @@
+/* eslint-disable no-alert */
 import { LightningElement, track } from "lwc";
-import search from "@salesforce/apex/EmailClass.search";
+import sendEmailController from "@salesforce/apex/EmailClass.sendEmailController";
 
 export default class EmailLwc extends LightningElement {
-    @track items = [];
-    searchTerm = "";
-    blurTimeout;
-    boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
-    @track selectedValues = [];
-    selectedValuesMap = new Map();
-
-    get hasItems() {
-        return this.items.length;
-    }
-
-    handleInputChange(event) {
-        if (event.target.value.length < 3) {
-            return;
+    toAddress = [];
+    ccAddress = [];
+    subject = "";
+    body = "";
+    @track files = [
+        {
+            contentVersionId: "0687F000010EtHsQAK",
+            documentId: "0697F00000wLFjQQAW",
+            name: "undraw_Travel_mode_re_2lxo.png"
         }
+    ];
 
-        this.searchTerm = event.target.value;
+    wantToUploadFile = false;
+    noEmailError = false;
+    invalidEmails = false;
 
-        search({ searchString: event.target.value })
-            .then((result) => {
-                console.log("Result", result);
-                this.items = result;
-                if (this.items.length > 0) {
-                    this.boxClass =
-                        "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open";
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-        //this.items = [];
+    toggleFileUpload() {
+        this.wantToUploadFile = !this.wantToUploadFile;
     }
 
-    handleBlur() {
-        console.log("In onBlur");
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        this.blurTimeout = setTimeout(() => {
-            this.boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
-            const value = this.searchTerm;
-            if (value !== undefined && value != null && value !== "") {
-                this.selectedValuesMap.set(value, value);
-                this.selectedValues = [...this.selectedValuesMap.keys()];
-            }
-
-            console.log("this.selectedValues***", this.selectedValues);
-            this.searchTerm = "";
-        }, 300);
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+        console.log("uploadedFiles", uploadedFiles);
+        this.files = [...this.files, ...uploadedFiles];
+        this.wantToUploadFile = false;
     }
 
     handleRemove(event) {
-        const item = event.target.label;
-        console.log("item", item);
-        this.selectedValuesMap.delete(item);
-        this.selectedValues = [...this.selectedValuesMap.keys()];
+        const index = event.target.dataset.index;
+        this.files.splice(index, 1);
     }
 
-    onSelect(event) {
-        this.searchTerm = "";
-        console.log("In onSelect");
-        let ele = event.currentTarget;
-        let selectedId = ele.dataset.id;
-        console.log("selectedId", selectedId);
-        let selectedValue = this.items.find((record) => record.Id === selectedId);
-        this.selectedValuesMap.set(selectedValue.Email, selectedValue.Name);
-        this.selectedValues = [...this.selectedValuesMap.keys()];
+    handleToAddressChange(event) {
+        this.toAddress = event.detail.selectedValues;
+    }
 
-        //As a best practise sending selected value to parent and inreturn parent sends the value to @api valueId
-        let key = this.uniqueKey;
-        const valueSelectedEvent = new CustomEvent("valueselect", {
-            detail: { selectedId, key }
-        });
-        this.dispatchEvent(valueSelectedEvent);
+    handleCcAddressChange(event) {
+        this.ccAddress = event.detail.selectedValues;
+    }
 
-        if (this.blurTimeout) {
-            clearTimeout(this.blurTimeout);
+    handleSubjectChange(event) {
+        this.subject = event.target.value;
+    }
+
+    handleBodyChange(event) {
+        this.body = event.target.value;
+    }
+
+    validateEmails(emailAddressList) {
+        console.log("emailAddressList", emailAddressList);
+        let areEmailsValid;
+        if(emailAddressList.length > 1) {
+            areEmailsValid = emailAddressList.reduce((accumulator, next) => {
+                console.log("acc", accumulator);
+                console.log("next", next);
+                const isValid = this.validateEmail(next);
+                return accumulator && isValid;
+            });
         }
-        this.boxClass = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus";
+        else if(emailAddressList.length > 0) {
+            areEmailsValid = this.validateEmail(emailAddressList[0]);
+        }
+
+        console.log("areEmailsValid", areEmailsValid);
+        return areEmailsValid;
+    }
+
+    validateEmail(email) {
+        console.log("In VE");
+        const res = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()s[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        console.log("res", res);
+        return res.test(String(email).toLowerCase());
+    }
+
+    handleReset() {
+        this.toAddress = [];
+        this.ccAddress = [];
+        this.subject = "";
+        this.body = "";
+        this.files = [];
+        this.template.querySelectorAll("c-email-input").forEach((input) => input.reset());
+    }
+
+    handleSendEmail() {
+        this.noEmailError = false;
+        this.invalidEmails = false;
+        if (![...this.toAddress, ...this.ccAddress].length > 0) {
+            this.noEmailError = true;
+            return;
+        }
+        
+        if (!this.validateEmails([...this.toAddress, ...this.ccAddress])) {
+            console.log("Some of the emails are invalid");
+            this.invalidEmails = true;
+            return;
+        }
+
+        console.log("Emails are valid");
+
+        let emailDetails = {
+            toAddress: this.toAddress,
+            ccAddress: this.ccAddress,
+            subject: this.subject,
+            body: this.body
+        };
+        console.log("emailDetails", emailDetails);
+
+        sendEmailController({ emailDetailStr: JSON.stringify(emailDetails) })
+            .then(() => {
+                console.log("Email Sent");
+            })
+            .catch((error) => {
+                console.error("Error in sendEmailController:", error);
+            });
     }
 }
